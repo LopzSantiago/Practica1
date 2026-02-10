23c9871f76d4a7a369e8df3cbb65bb041df90775
Zair Santiago Lopez Luna <zaisantiagolopezluna.07@gmail.com>
2026-02-09 20:17:29 -0600

    primer commit con los codigos sin compilar

---
+++ src/ito/poo/App.java
@@ -0,0 +1,13 @@
+package ito.poo;
+
+public class App {
+    static Mascota mascota;
+
+    static void run(){
+        mascota= new Mascota("satur", 2, "perro", "chihuahua");
+        System.out.println(mascota);
+        mascota.cumpleAños();
+        System.out.println("Despues de cumplir años"+mascota);
+    }
+    public static void main (String[] args){run();}
+}
